3e126d0cdb5dc54d538133f31fc2cd874da2b81b
pkashyap148 <pkashyap148@gmail.com>
2020-07-07 23:02:14 +0530

    GitHub Automation

---
+++ github.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+u = input("Enter Email: ")
+p = input("Enter Password: ")
+
+driver = webdriver.Chrome()
+driver.get("https://github.com/login")
+
+username = driver.find_element_by_name('login')
+username.click()
+username.send_keys(u)
+
+pwd = driver.find_element_by_name('password')
+pwd.click()
+pwd.send_keys(p)
+
+submit = driver.find_element_by_name('commit')
+submit.click()
+
+x = input("Enter Verification code: ")
+
+otp = driver.find_element_by_name('otp')
+otp.click()
+otp.send_keys(x)
+
+log = driver.find_element_by_xpath('//*[@id="login"]/div[3]/form/button')
+log.click()
+
+time.sleep(10)
+out = driver.get_element_by_xpath('/html/body/div[1]/header/div[3]/nav/form/button')
+out.click()
